8b12de61c762d7c7f8d4afae2f552d7554f9156c
Parth Patel <parth.pnp@gmail.com>
2017-09-08 21:31:24 +0530

    [ADD] Initial Deploy

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './templates/dashboard.html',
+  styleUrls: [
+  	'./assets/css/material-dashboard.css',
+  	'./assets/css/demo.css',
+
+  ]
+})
+export class AppComponent {
+}
